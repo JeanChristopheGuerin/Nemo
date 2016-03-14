768cd7d8ec8491c0aaaa1ab3cf474197b705a3af
jeanchristopheguerin <jcgmoaa@gmail.com>
2016-03-14 18:03:54 +0100

    aquarium

---
+++ src/implementations/Aquarium.java
@@ -0,0 +1,65 @@
+package implementations;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import interfaces.IAquarium;
+import interfaces.IPoisson;
+
+public class Aquarium implements IAquarium{
+private int width;
+private int length;
+private int speed;
+private Color waterColor;
+private ArrayList<? extends IPoisson> poissons;
+
+	public void addPoisson(IPoisson poisson) {
+		
+		
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public int getSpeed(int vitesse) {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public Color getWaterColor() {
+		return waterColor;
+	}
+
+	public void setWaterColor(Color waterColor) {
+		this.waterColor = waterColor;
+	}
+
+	public ArrayList<? extends IPoisson> getPoissons() {
+		return poissons;
+	}
+
+	public void setPoissons(ArrayList<? extends IPoisson> poissons) {
+		this.poissons = poissons;
+	}
+
+	
+
+
+
+}
--- src/interfaces/IAquarium.java
+++ src/interfaces/IAquarium.java
@@ -1,5 +1,13 @@
 package interfaces;
 
+import java.awt.Color;
+
 public interface IAquarium {
 
+	
+void addPoisson(IPoisson poisson);	
+void setWaterColor(Color couleur);
+void setWidth(int largeur);
+void setLength(int longueur);
+void setSpeed(int vitesse);
 }
